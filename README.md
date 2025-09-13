69c354baacd1da4bb72676fe185e9966d0994a56
Gabriel Vieira <gabrielcnt96@gmail.com>
2025-09-12 22:17:54 -0300

    atividade 1 finalizada

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repositório para exercicios e atividades do BFD
+Este repositório tem como propósito armazenar todas as atividades passadas no curso
+BFD 
---
+++ atividade_final_modulo_1/verificar_elementos_da_lista.py
@@ -0,0 +1,6 @@
+lista = [1, 3, 4, 5, 6] # lista com elementos
+
+if 2 not in lista: # condição que verifica se o valor 2 não está na lista
+    print('o valor 2 não está na lista')
+else:
+    print('o valor 2 esta na lista')
